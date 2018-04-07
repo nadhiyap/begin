42eb7276d71eb3f2c1adfa28444715a1ec762105
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-04-07 12:21:03 +0530

    bet.py

---
+++ bet.py
@@ -0,0 +1,12 @@
+a=int(input("enter a string "))
+n=int(input("enter a string "))
+l=int(input("enter a string "))
+c=0
+for i in range(n,l):
+    if a==i:
+        c=1
+if c==1:
+    print("yes")
+else:
+    print("no")
+    
