d3280ba0b92b5f953caa6a094208cd4ac08680cc
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-04-15 10:40:43 +0530

    exist.py

---
+++ exist.py
@@ -0,0 +1,13 @@
+a=0
+c=0
+b=1
+n=int(input("enter the n"))
+k=int(input("enter the n"))
+for i in range(n):
+    a=int(input())
+    if a==k:
+        b=0
+if b==0:
+    print("yes")
+else:
+     print("no")
